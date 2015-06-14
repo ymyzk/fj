a2ce9aefcf22da30bed72d93303341b47c3a0438
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-06-15 04:00:14 +0900

    Add check_class_super

--- typing.ml
+++ typing.ml
@@ -23,6 +23,13 @@ let base_class = {
   methods = [];
 }
 
+(* get_class : ClassTable -> id -> klass *)
+let get_class table name =
+  try
+    ClassTable.find name table
+  with Not_found ->
+    raise (Type_error ("the class is not found in table: " ^ name))
+
 (* create_classtable : klass list -> ClassTable *)
 let create_classtable classes =
   let table = ClassTable.singleton base_class.name base_class in
@@ -34,9 +41,29 @@ let create_classtable classes =
       ClassTable.add name klass tb
   end table classes
 
+(* check_class_super : ClassTable -> id -> unit *)
+let check_class_super table name =
+  let rec check_class_super table name1 name2 =
+    if name1 = name2 then
+      raise (Type_error ("cyclic inheritance involving " ^ name1))
+    else if name1 = base_class_name || name2 = base_class_name then
+      ()
+    else
+      check_class_super table name1 ((get_class table name2).super)
+  in
+    check_class_super table name ((get_class table name).super)
+
+(* check_class : ClassTable -> klass -> unit *)
+let check_class table klass =
+  check_class_super table klass.name
+
+(* check_classtable : ClassTable -> unit *)
+let check_classtable table =
+  ClassTable.iter (fun _ k -> check_class table k) table
+
 (* check : klass list -> unit *)
 let check classes =
   let table = create_classtable classes in
   (* For debugging *)
   ClassTable.iter (fun name klass -> print_endline name) table;
-  ()
+  check_classtable table
