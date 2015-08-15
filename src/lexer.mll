bf16bcba622818ce8d8b2b787aa0b133bcc42926
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-08-15 16:06:52 +0900

    Update lexer/parser error reporting

--- src/lexer.mll
+++ src/lexer.mll
@@ -1,6 +1,8 @@
 {
 open Lexing
 
+exception Lexer_error
+
 let reservedWords = [
   (* Keywords *)
   ("class", Parser.CLASS);
@@ -10,10 +12,18 @@ let reservedWords = [
   ("super", Parser.SUPER);
   ("this", Parser.THIS);
 ]
+
+let increment_linenum lexbuf =
+  let pos = lexbuf.Lexing.lex_curr_p in
+  lexbuf.Lexing.lex_curr_p <- { pos with
+  Lexing.pos_lnum = pos.Lexing.pos_lnum + 1;
+  Lexing.pos_bol = pos.Lexing.pos_cnum;
+}
 }
 
 rule main = parse
-  [' ' '\009' '\012' '\n']+     { main lexbuf }
+  [' ' '\009' '\012']+     { main lexbuf }
+| '\n' { increment_linenum lexbuf; main lexbuf }
 | "," { Parser.COMMA }
 | "." { Parser.PERIOD }
 | "=" { Parser.EQ }
@@ -27,8 +37,7 @@ rule main = parse
     try
       List.assoc id reservedWords
     with
-(*       _ -> print_endline (Printf.sprintf "ID: %s l: %d" id (Lexing.lexeme_start lexbuf)); *)
-       _ -> print_endline (Printf.sprintf "ID: %s l: %d" id ((Lexing.lexeme_start_p lexbuf).pos_lnum)); 
-      Parser.ID id
+      Not_found -> Parser.ID id
   }
 | eof { Parser.EOF }
+| _ { raise Lexer_error }
--- src/main.ml
+++ src/main.ml
@@ -1,18 +1,38 @@
+open Printf
 open Typing
+
+let print_error file lexbuf message =
+  let start = Lexing.lexeme_start_p lexbuf in
+  let line = start.Lexing.pos_lnum in
+  eprintf "%s:%d: %s\n" file line message
 
 (* 1つのファイルを読み込んでパース処理を行い, Type checker にかける *)
 let read_from_file file =
   print_endline ("Reading from " ^ file);
   let ic = open_in file in
+    let lexbuf = Lexing.from_channel ic in
     try
-      let classes = Parser.toplevel Lexer.main (Lexing.from_channel ic) in
+      let classes = Parser.toplevel Lexer.main lexbuf in
         print_endline "Running type check...";
         check classes;
         print_endline "Completed"
     with e ->
-      close_in_noerr ic;
-      print_endline "Error";
-      raise e
+      begin match e with
+        Failure message ->
+          print_endline ("Failure: " ^ message)
+      | Lexer.Lexer_error ->
+          let token = Lexing.lexeme lexbuf in
+          let message = sprintf "lexer error: unexpected token '%s'" token in
+          print_error file lexbuf message
+      | Parsing.Parse_error | Parser.Error -> (* ocamlyacc & Menhir *)
+          let token = Lexing.lexeme lexbuf in
+          let message = sprintf "parser error: unexpected token '%s'" token in
+          print_error file lexbuf message
+      | _ ->
+        close_in_noerr ic;
+        print_endline "unknown error";
+        raise e
+      end
 
 let _ =
   let files = ref [] in
