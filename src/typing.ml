97618078b10b34b436966a1f9b2e8db4c5977f87
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-08-15 16:50:34 +0900

    Refactor typing

--- src/typing.ml
+++ src/typing.ml
@@ -23,36 +23,58 @@ let base_class = {
   methods = [];
 }
 
-(* get_class : Class.t Environment.t -> id -> Class.t *)
+(* クラスのリストからクラステーブルを作成する *)
+(* Class.t list -> Class.t Environment.t *)
+let create_classtable classes =
+  let table = Environment.singleton (Class.name base_class) base_class in
+  List.fold_left begin fun tb klass ->
+    let name = Class.name klass in
+    if Environment.mem name tb then
+      (* 同じ名前のクラス名が複数あればエラー *)
+      raise (Type_error ("duplicate class: " ^ name))
+    else
+      Environment.add name klass tb
+  end table classes
+
+(* クラステーブルからクラスを取得する *)
+(* Class.t Environment.t -> Type.t -> Class.t *)
 let get_class table name =
   try
     Environment.find name table
   with Not_found ->
     raise (Type_error ("the class is not found in table: " ^ name))
 
+(* クラステーブルからスーパークラスを取得する *)
+(* Class.t Environment.t -> Class.t -> Class.t *)
 let super_of table klass =
   get_class table (Type.name (Class.super klass))
 
 (* k1 は k0 のサブクラスかどうか *)
+(* Class.t Environment.t -> Type.t -> Type.t -> bool *)
 let rec is_subclass table k0 k1 =
   if k0 = k1 then
+    (* C <: C *)
     true
   else if k1 = base_class_name then
+    (* Object は Object 以外のサブクラスではない *)
     false
   else
+    (* (class C extends D -> C <: D) & D <: E -> C <: E より,
+     * D <: E をチェックする *)
     is_subclass table k0 (Class.super (get_class table k1))
 
-(* l1 と l0 のそれぞれ n 番目の要素 k1, k0 について, k1 は k0 のサブクラスかどうか *)
-(* is_subclasses : Class.t Environment.t -> Type.t list -> Type.t list -? bool *)
+(* l1 と l0 の n 番目の要素 k1, k0 について, k1 は k0 のサブクラスかどうか *)
+(* Class.t Environment.t -> Type.t list -> Type.t list -> bool *)
 let rec is_subclasses table l0 l1 =
   match l0, l1 with
   | [], [] -> true
-  | [], _ -> false
-  | _, [] -> false
+  | [], _  -> false
+  | _ , [] -> false
   | (k0 :: ks0), (k1 :: ks1) ->
       (is_subclass table k0 k1) && (is_subclasses table ks0 ks1)
 
-(* get_field : Class.t -> id -> Class.t *)
+(* クラスからフィールドを取得する処理 *)
+(* Class.t Environment.t -> Class.t -> id -> Field.t *)
 let get_field table klass name =
   let rec get_field table klass' name =
     try
@@ -64,7 +86,8 @@ let get_field table klass name =
       get_field table super_klass name in
   get_field table klass name
 
-(* get_method : Class.t -> id -> Class.t *)
+(* クラスからメソッドを取得する処理 *)
+(* Class.t Environment.t -> Class.t -> id -> Method.t *)
 let get_method table klass name =
   let rec get_method table klass' name =
     try
@@ -76,18 +99,8 @@ let get_method table klass name =
       get_method table super_klass name in
   get_method table klass name
 
-(* create_classtable : Class.t list -> Class.t Environment.t *)
-let create_classtable classes =
-  let table = Environment.singleton (Class.name base_class) base_class in
-  List.fold_left begin fun tb klass ->
-    let name = Class.name klass in
-    if Environment.mem name tb then
-      raise (Type_error ("duplicate class: " ^ name))
-    else
-      Environment.add name klass tb
-  end table classes
-
-(* check_exp : Class.t Environment.t -> Type.t Environment.t -> Type.t *)
+(* 式の型を求める処理 *)
+(* Class.t Environment.t -> Type.t Environment.t -> exp -> Type.t *)
 let rec check_exp table env = function
   | Var(n0) when Environment.mem n0 env ->
       Environment.find n0 env
@@ -97,7 +110,7 @@ let rec check_exp table env = function
       let k0 = get_class table (check_exp table env e0) in
       let f0 = get_field table k0 n0 in
       Field.ty f0
-  | FieldSet(e0, n0, e1) -> (* e0.n0 = e1; *)
+  | FieldSet(e0, n0, e1) -> (* e0.n0 = e1 *)
       begin match e0, n0, e1 with
       | (Var "this"), _, (Var _) ->
         let k0 = get_class table (check_exp table env e0) in (* e0 (this) : k0 *)
@@ -142,13 +155,15 @@ let rec check_exp table env = function
       else
         (* stupid cast *)
         begin
-        print_endline (
-          "stupid cast from class '" ^ (Type.name t1)
-          ^ "' to class '" ^ (Type.name t0) ^ "'");
-        t0
+          (* エラーメッセージを出力するが処理は続ける *)
+          Printf.eprintf
+            "stupid cast from class '%s' to class '%s'"
+            (Type.name t1) (Type.name t0);
+          t0
         end
 
-(* check_class_super : Class.t Environment.t -> id -> unit *)
+(* クラスの部分型関係のチェック, 不完全? 不要? *)
+(* Class.t Environment.t -> id -> unit *)
 let check_class_super table name =
   let rec check_class_super table name1 name2 =
     if name1 = base_class_name || name2 = base_class_name then
@@ -163,16 +178,16 @@ let check_class_super table name =
   in
     check_class_super table name (Class.super (get_class table name))
 
-(* check_field : Type.t Environment.t -> Class.t -> Field.t -> unit *)
 (* フィールド名の重複チェック *)
+(* Type.t Environment.t -> Class.t -> Field.t -> unit *)
 let check_field env klass field =
   let name = Field.name field in
   if Environment.mem name env then
     raise (Type_error ("variable " ^ name ^ " is already defined in class " ^ (Class.name klass)));
   ()
 
-(* check_fields : Type.t Environment.t -> Class.t -> Type.t Environment.t *)
 (* クラスのフィールドのチェックと env へのフィールドの追加 *)
+(* Type.t Environment.t -> Class.t -> Type.t Environment.t *)
 let check_fields env klass =
   List.fold_left begin
     fun env' field ->
@@ -182,6 +197,8 @@ let check_fields env klass =
       Environment.add name ty env'
   end env (Class.fields klass)
 
+(* 初期化されていないフィールドがないかチェック *)
+(* Class.t -> unit *)
 let check_uninitialized_fields klass =
   let constructor_fields =
     List.map
@@ -190,21 +207,23 @@ let check_uninitialized_fields klass =
   let constructor_fields = List.sort compare constructor_fields in
   let class_fields = List.map (fun f -> Field.name f) (Class.fields klass) in
   let class_fields = List.sort compare class_fields in
+  (* コンストラクタ内で初期化されているフィールドの名前のリストと
+   * クラスで定義されているフィールドの名前のリストを比較 *)
   if constructor_fields <> class_fields then
     raise (Type_error ("uninialized fields in class " ^ (Class.name klass)))
   else
     ()
 
 (* コンストラクタのパラメーターを左から順に環境に追加 *)
-(* 追加する際にパラメータ名の重複チェックを行う *)
-(* check_constructor_parameters : Type.t Environment.t -> Constructor.t -> Type.t Environment.t *)
+(* Type.t Environment.t -> Constructor.t -> Type.t Environment.t *)
 let check_constructor_parameters env constructor =
   let parameters = Constructor.parameters constructor in
   List.fold_left
     (fun e (k, n) -> Environment.add k n e) env parameters
 
 (* すべてのパラメータがフィールドの初期化かスーパークラスのコンストラクタ呼び出しに利用されているかをチェック *)
-let check_constructor_parameters_used env constructor =
+(* Constructor.t -> unit *)
+let check_constructor_parameters_used constructor =
   let parameters =
       List.map (fun (n, _) -> n) (Constructor.parameters constructor) in
   let parameters = List.sort compare parameters in
@@ -226,18 +245,21 @@ let check_constructor_parameters_used env constructor =
     ()
 
 (* コンストラクタ本体 (フィールドの初期化) の型チェック *)
+(* Class.t Environment.t Type.t Environment.t Constructor.t -> unit *)
 let check_constructor_fields table env constructor =
   List.iter
     (fun e -> ignore (check_exp table env e))
     (Constructor.body constructor)
 
 (* スーパークラスのコンストラクタ呼び出しの型チェック *)
+(* Class.t Environment.t -> Type.t Environment.t -> Class.t -> unit *)
 let check_constructor_super table env klass =
   let constructor = Class.constructor klass in
-  (* クラスの型が Object でなければ *)
   if Class.ty klass = Class.ty base_class then
+    (* Object クラスはスーパークラスのコンストラクタを呼び出さない *)
     ()
   else
+    (* クラスの型が Object でなければ *)
     (* スーパークラスを取得 *)
     let super_klass = super_of table klass in
     (* スーパークラスのコンストラクタに渡される引数を取得 *)
@@ -260,7 +282,8 @@ let check_constructor_super table env klass =
         "cannot invoke a super class constructor in the constructor of class "
         ^ (Class.name super_klass)))
 
-(* check_constructor : Class.t Environment.t -> Type.t Environment.t -> Class.t -> unit *)
+(* コンストラクタのチェック *)
+(* Class.t Environment.t -> Type.t Environment.t -> Class.t -> unit *)
 let rec check_constructor table env klass =
   let constructor = Class.constructor klass in
   (* コンストラクタの名前はクラスの名前と同じ *)
@@ -273,9 +296,11 @@ let rec check_constructor table env klass =
   (* スーパークラスコンストラクタ呼び出しのチェック *)
   check_constructor_super table env' klass;
   (* 全てのパラメータが重複なく利用されているかチェック *)
-  check_constructor_parameters_used env constructor;
+  check_constructor_parameters_used constructor;
   ()
 
+(* メソッドのチェック *)
+(* Class.t Environment.t Type.t Environment.t -> Method.t -> unit *)
 let check_method table env meth =
   (* パラメータを環境に追加 *)
   let env' =
@@ -290,10 +315,13 @@ let check_method table env meth =
   else
     raise (Type_error ("return type error"))
 
+(* クラスのすべてのメソッドのチェック *)
+(* Class.t Environment.t -> Type.t Environment.t -> Class.t -> unit *)
 let check_methods table env klass =
   List.iter (fun m -> check_method table env m) (Class.methods klass)
 
-(* check_class : Class.t Environment.t -> Class.t Environment.t -> Class.t -> unit *)
+(* クラスのチェック *)
+(* Class.t Environment.t -> Type.t Environment.t -> Class.t -> unit *)
 let check_class table env klass =
   check_class_super table (Class.name klass);
   let env' = Environment.add "this" (Class.ty klass) env in
@@ -306,14 +334,14 @@ let check_class table env klass =
   (* メソッドのチェック *)
   check_methods table env' klass
 
-(* check_classtable : Class.t Environment.t -> unit *)
+(* クラステーブルのクラスを順にチェック *)
+(* Class.t Environment.t -> unit *)
 let check_classtable table =
   let env = Environment.empty in
   Environment.iter (fun _ k -> check_class table env k) table
 
+(* クラスのリストを順にチェック *)
 (* check : Class.t list -> unit *)
 let check classes =
   let table = create_classtable classes in
-  (* For debugging *)
-(*   Environment.iter (fun name klass -> print_endline name) table; *)
   check_classtable table
