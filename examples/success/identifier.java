e4bd0c08ebf15bb2b7b1dcedb7aec0774b5326e8
Yusuke Miyazaki <miyazaki.dev@gmail.com>
2015-08-19 13:22:11 +0900

    Add identifier example

---
+++ examples/success/identifier.java
@@ -0,0 +1,13 @@
+class A extends Object {
+    A() {
+        super();
+    }
+
+    Object method_1(Object o) {
+        return o;
+    }
+
+    Object $method_1(Object o) {
+        return o;
+    }
+}
